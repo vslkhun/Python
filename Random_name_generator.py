8edd8300a30590c16f37240217d6bbe337856f82
vslkhun <vslkhun@gmail.com>
2024-08-31 12:47:32 +0530

    Template for random name generator

---
+++ Random_name_generator.py
@@ -0,0 +1,36 @@
+import random
+
+# Define vowels and consonants
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+
+# Define syllable patterns
+syllable_patterns = [
+    "V",      # Vowel
+    "C",      # Consonant
+    "VC",     # Vowel + Consonant
+    "CV",     # Consonant + Vowel
+    "CVC"     # Consonant + Vowel + Consonant
+]
+
+def generate_syllable(pattern:str)->str:
+    #accepts a  syllable pattern and returns a syllable
+
+    """Your code here"""
+
+    return syllable
+
+def generate_name(length:int)->str:
+    #Max lenth of the name
+    #using random generator select a random syllable pattern
+    #generate sylabble by calling generate_syllable
+    #append to name untill the desire length
+    name:str = ""
+    
+    """Your code here"""
+
+    return name
+
+# main function
+def main()->None:
+    #get user input and call generate_name
